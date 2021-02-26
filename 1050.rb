85518e5e779184a4136065d51745402f24472975
Gerlane <gerlanebarbosaandrade.gerlane@gmail.com>
2021-02-26 16:21:28 -0300

    codigo

---
+++ 1050.rb
@@ -0,0 +1,24 @@
+num = gets.strip.to_i
+
+if num == 61
+    puts "Brasilia"
+elsif num == 71
+    puts "Salvador"
+elsif num == 11
+    puts "Sao Paulo"
+elsif num == 21 
+    puts "Rio de Janeiro"
+elsif num == 32
+    puts "Juiz de Fora"
+elsif num == 19
+    puts "Campinas"
+elsif num == 27
+    puts "Vitoria"
+elsif num == 31
+    puts "Belo Horizonte"
+ else puts "DDD nao cadastrado"
+
+ end
+
+
+
